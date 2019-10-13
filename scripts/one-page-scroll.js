0a9a73b9f614b3c5949da15f01573f4a0059e789
savantm1 <savantm1@mail.ru>
2019-10-13 16:21:17 +0300

    Доработки с секцией плеера

---
+++ scripts/one-page-scroll.js
@@ -0,0 +1,118 @@
+// one page scroll
+
+const sections = $(".section");
+const display = $('.one-page-scroll');
+let inScroll = false;
+
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
+const countPositionPercent = sectionEq => {
+  return `${sectionEq * -100}%`;
+};
+
+const switchActiveClass = (elems, elemEq) => {
+  elems
+    .eq(elemEq)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+};
+
+const unBlockScroll = () => {
+  const transitionDuration = 1000;
+  const touchScrollInertionTime = 300;
+
+  setTimeout(() => {
+    inScroll = false;
+  }, transitionDuration + touchScrollInertionTime);
+};
+
+
+const performTransition = sectionEq => {
+  if (inScroll) return;
+
+  inScroll = true;
+  const position = countPositionPercent(sectionEq);
+  const switchFixedMenuClass = () => switchActiveClass($(".paginator__link"), sectionEq);
+
+  switchFixedMenuClass();
+  switchActiveClass(sections, sectionEq);
+
+  display.css({
+    transform: `translateY(${position})`
+  });
+
+  unBlockScroll();
+};
+
+const scrollViewport = direction => {
+  const activeSection = sections.filter(".active");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+
+  if (direction === "next" && nextSection.length) {
+    performTransition(nextSection.index());
+  }
+
+  if (direction === "prev" && prevSection.length) {
+    performTransition(prevSection.index());
+  }
+};
+
+$(document).on({
+  wheel: e => {
+    const deltaY = e.originalEvent.deltaY;
+    const direction = deltaY > 0 ? "next" : "prev";
+    scrollViewport(direction);
+  },
+  keydown: e => {
+    const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInputs = tagName === "input" || tagName === "textarea";
+
+    if (userTypingInInputs) return;
+
+    switch (e.keyCode) {
+      case 40:
+        scrollViewport("next");
+        break;
+
+      case 38:
+        scrollViewport("prev");
+        break;
+    }
+  }
+});
+
+$('.wrapper').on('touchmove', e => {
+  e.preventDefault();
+});
+
+
+$("[data-scroll-to]").on("click", e => {
+  e.preventDefault();
+  performTransition(parseInt($(e.currentTarget).attr("data-scroll-to")));
+});
+
+
+// разрешаем свайп на мобильниках
+if (isMobile) {
+  window.addEventListener(
+    "touchmove",
+    e => {
+      e.preventDefault();
+    }, {
+      passive: false
+    }
+  );
+
+  $("body").swipe({
+    swipe: (event, direction) => {
+      let scrollDirecrion;
+      if (direction === "up") scrollDirecrion = "next";
+      if (direction === "down") scrollDirecrion = "prev";
+      scrollViewport(scrollDirecrion);
+    }
+  });
+}
--- scripts/player.js
+++ scripts/player.js
@@ -7,45 +7,11 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
+
 var player;
 
-const eventsInit = () => {
-  $('.player__start')
-}
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    },
-    // playerVars: {
-    //   controls: 0,
-    //   disablekb: 0,
-    //   showinfo: 0,
-    //   rel: 0,
-    //   autoplay: 0,
-    //   modestbranding: 0
-    // }
+    videoId: 'dQw4w9WgXcQ'
   });
-}
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-
-function stopVideo() {
-  player.stopVideo();
 }
